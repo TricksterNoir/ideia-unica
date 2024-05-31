8d110376bcb380634b9f863cb28c3e05023fbbd5
SylvioLabriola <sylvioez@gmail.com>
2024-05-31 18:11:57 -0300

    feat: removed the lock archives

--- app/(website)/home.js
+++ app/(website)/home.js
@@ -22,11 +22,18 @@ export default function Post({ posts }) {
               <PostList key={post._id} post={post} aspect="square" />
             ))}
           </div>
+
+            <div className="bg-blue-200">
+              <h1 className="display:flex text-center">TESTANDO ESSA CACETA</h1>
+            </div>
+
           <div className="mt-10 flex justify-center">
             <Link
               href="/archive"
-              className="relative inline-flex items-center gap-1 rounded-md border border-gray-300 bg-white px-3 py-2 pl-4 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20 disabled:pointer-events-none disabled:opacity-40 dark:border-gray-500 dark:bg-gray-800 dark:text-gray-300">
-              <span>View all Posts</span>
+              className="relative inline-flex items-center gap-1 rounded-md border border-gray-300 bg-white px-3 py-2 pl-4 text-sm 
+              font-medium text-gray-500 hover:bg-gray-50 focus:z-20 disabled:pointer-events-none disabled:opacity-40 dark:border-gray-500 
+              dark:bg-gray-800 dark:text-gray-300">
+              <span>Ver mais</span>
             </Link>
           </div>
         </Container>
--- pages/_app.js
+++
@@ -1,7 +0,0 @@
-import './style.css'; // Importa o arquivo CSS global
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
